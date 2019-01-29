e7020410fc54f50fee130de7887c7c538c7e5d06
Umer Hassam <umer.hassam@gmail.com>
2019-01-29 22:46:30 +0500

    added task menu to projects index page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,7 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+//
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap.min   // Add it after jquery and jquery_ujs
